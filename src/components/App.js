0e2831959b2534ddb658ae9c4f0d9b59e5615511
NishantBadlani <nishant.03badlani@gmail.com>
2020-11-07 00:08:23 +0530

    Commit2

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.relatives = ["Name1", "Name2", "Name3", "Name4"];
+  }
 
-    constructor() {
-        super();
-        this.relatives = ["Name1", "Name2", "Name3", "Name4"];
-    }
-
-    render() {
-
-        return(
-            <div id="main">
-               <ol key="relativeList">
-                {this.relatives.map((relative, index) => <li key={`relativeListItem${index+1}`}>{relative}</li>)}
-               </ol>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {this.relatives.map((relative, index) => (
+            <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
