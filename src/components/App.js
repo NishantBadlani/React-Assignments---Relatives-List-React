253388b0c8a2bb02a1e135f8275b5ec26c68ba7d
NishantBadlani <nishant.03badlani@gmail.com>
2020-11-07 00:07:47 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,19 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.relatives = ["Name1", "Name2", "Name3", "Name4"];
+    }
+
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <ol key="relativeList">
+                {this.relatives.map((relative, index) => <li key={`relativeListItem${index+1}`}>{relative}</li>)}
+               </ol>
             </div>
         )
     }
